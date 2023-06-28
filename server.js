72df1e3d15f197064d5807de500af05dfc027026
Abiboulayed <79129877+Hackerbibou@users.noreply.github.com>
2023-06-28 11:07:28 -0400

    deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+const cors = require('cors')
 // Always require and configure near the top
 require('dotenv').config();
 // Connect to the database
@@ -17,6 +18,11 @@ app.use(express.json());
 // to serve from the production 'build' folder
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(cors({
+  origin:['http://localhost:3000','https://shopflix.onrender.com']
+
+}))
 
 app.use(require('./config/checkToken'));
 
