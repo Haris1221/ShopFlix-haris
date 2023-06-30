9d251b85793eaa38eb798c049f9c1b2d4ea691a5
haris <Haris199x@gmail.com>
2023-06-30 01:06:59 -0500

    My list implementation done by haris coauthored by habib and andrew

--- models/order.js
+++ models/order.js
@@ -4,7 +4,6 @@ const itemSchema = require("./itemSchema");
 
 const lineItemSchema = new Schema(
   {
-    qty: { type: Number, default: 1 },
     item: itemSchema,
   },
   {
@@ -14,8 +13,7 @@ const lineItemSchema = new Schema(
 );
 
 lineItemSchema.virtual("extPrice").get(function () {
-  // 'this' is bound to the lineItem subdoc
-  return this.qty * this.item.price;
+  return this.item.price;
 });
 
 const orderSchema = new Schema(
@@ -34,58 +32,21 @@ orderSchema.virtual("orderTotal").get(function () {
   return this.lineItems.reduce((total, item) => total + item.extPrice, 0);
 });
 
-orderSchema.virtual("totalQty").get(function () {
-  return this.lineItems.reduce((total, item) => total + item.qty, 0);
-});
-
-orderSchema.virtual("orderId").get(function () {
-  return this.id.slice(-6).toUpperCase();
-});
-
 orderSchema.statics.getCart = function (userId) {
-  // 'this' is the Order model
   return this.findOneAndUpdate(
-    // query
     { user: userId, isPaid: false },
-    // update
     { user: userId },
-    // upsert option will create the doc if
-    // it doesn't exist
     { upsert: true, new: true }
   );
 };
 
 orderSchema.methods.addItemToCart = async function (itemId) {
   const cart = this;
-  // Check if item already in cart
   const lineItem = cart.lineItems.find((lineItem) =>
     lineItem.item._id.equals(itemId)
   );
-  if (lineItem) {
-    lineItem.qty += 1;
-  } else {
     const item = await mongoose.model("Item").findById(itemId);
     cart.lineItems.push({ item });
-  }
-  return cart.save();
-};
-
-// Instance method to set an item's qty in the cart (will add item if does not exist)
-orderSchema.methods.setItemQty = function (itemId, newQty) {
-  // this keyword is bound to the cart (order doc)
-  const cart = this;
-  // Find the line item in the cart for the menu item
-  const lineItem = cart.lineItems.find((lineItem) =>
-    lineItem.item._id.equals(itemId)
-  );
-  if (lineItem && newQty <= 0) {
-    // Calling remove, removes itself from the cart.lineItems array
-    lineItem.remove();
-  } else if (lineItem) {
-    // Set the new qty - positive value is assured thanks to prev if
-    lineItem.qty = newQty;
-  }
-  // return the save() method's promise
   return cart.save();
 };
 
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -6,9 +6,10 @@ import AuthPage from '../AuthPage/AuthPage';
 import NavBar from '../../components/NavBar/NavBar';
 import Movies from '../Movies/Movies'
 import MenuBar from '../../components/MenuBar/MenuBar';
+import myList from '../myList/myList';
 export default function App() {
   const [user, setUser] = useState(getUser());
-
+  const [cart, setCart] = useState([])
   return (
     <main className="">
       { user ?
@@ -16,7 +17,9 @@ export default function App() {
             <MenuBar/>
             <NavBar user={user} setUser={setUser} />
             <Routes>
-              <Route path='/' element={<Movies/>}/>
+              <Route path='/' element={<Movies setCart={setCart} cart={cart}/>}/>
+              <Route path='/orders' element={<myList cart={cart}/>}/>
+              
               {/* Route components in here */}
               {/* <Route path="/orders/new" element={<NewOrderPage />} />
               <Route path="/orders" element={<OrderHistoryPage />} /> */}
